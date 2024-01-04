4dd758f2715087a335ceaade8be5adffc490078e
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-01-04 02:39:57 +0200

    wip

--- commands/MakeLivewire.ts
+++ commands/MakeLivewire.ts
@@ -1,4 +1,4 @@
-import { BaseCommand, args } from '@adonisjs/core/build/standalone'
+import { BaseCommand, args, flags } from '@adonisjs/core/build/standalone'
 import { join } from 'path'
 
 export default class MakeLivewire extends BaseCommand {
@@ -14,16 +14,51 @@ export default class MakeLivewire extends BaseCommand {
     @args.string({ description: 'Name of component' })
     public name: string
 
+    @flags.boolean()
+    public inline: boolean
+
     public async run() {
+        this.inline ? await this.generateInline() : await this.generate()
+    }
+
+    private async generateInline() {
         this.generator
             .addFile(this.name + ".ts", {
                 pattern: "pascalcase",
             })
-            .stub(join(__dirname, '..', 'templates', 'livewire.txt'))
+            .stub(join(__dirname, '..', 'templates', 'livewire-inline.txt'))
             .useMustache()
             .destinationDir('app/Livewire')
             .appRoot(this.application.appRoot)
-            
+
+        await this.generator.run()
+    }
+
+    private async generate() {
+        this.generator
+            .addFile(this.name, {
+                pattern: "pascalcase",
+                extname: ".ts",
+            })
+            .stub(join(__dirname, '..', 'templates', 'livewire-component.txt'))
+            .useMustache()
+            .destinationDir('app/Livewire')
+            .appRoot(this.application.appRoot)
+
+        await this.generator.run()
+
+        this.generator.clear()
+
+        this.generator
+            .addFile(this.name, {
+                pattern: "dashcase",
+                extname: ".edge",
+            })
+            .stub(join(__dirname, '..', 'templates', 'livewire-template.txt'))
+            .useMustache()
+            .destinationDir('resources/views/livewire')
+            .appRoot(this.application.appRoot)
+
         await this.generator.run()
     }
 }
