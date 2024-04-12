049210a7b477a038b32af6df8767177a6fc88788
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-04-12 21:14:59 +0300

    wip

--- src/livewire.ts
+++ src/livewire.ts
@@ -10,7 +10,6 @@ import Computed from './features/support_computed/computed.js'
 import { SupportLazyLoading } from './features/support_lazy_loading/support_lazy_loading.js'
 import { Secret } from '@adonisjs/core/helpers'
 import type { Config } from './define_config.js'
-import { resolve } from 'node:path'
 
 export default class Livewire {
   app: ApplicationService
@@ -205,12 +204,10 @@ export default class Livewire {
         .map((s) => string.snakeCase(s))
         .join('/')
 
-      LivewireComponent = await import(resolve(process.cwd(), `./app/livewire/${path}.js`))
+      LivewireComponent = await import(`./app/livewire/${path}.js`)
         .then((module) => module.default)
         .catch(async () => {
-          return await import(resolve(process.cwd(), `./app/livewire/${path}/index.js`)).then(
-            (module) => module.default
-          )
+          return await import(`./app/livewire/${path}/index.js`).then((module) => module.default)
         })
     }
 
