e062c56bce2c2ffdc31eae53666c7a025ef3258d
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2026-05-20 18:45:15 +0300

    chore(pkg): bump version to 1.0.0-beta.0

--- src/component.ts
+++ src/component.ts
@@ -1,7 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { BaseComponent } from './base_component.js'
 import { HandlesJsEvaluation } from './features/support_js_evaluation/handles_js_evaluation.js'
-import { HandlesPageComponents } from './features/support_page_components/handles_page_components.js'
 import { HandlesDecorators } from './features/support_decorators/handles_decorators.js'
 import { HandlesRedirects } from './features/support_redirects/handles_redirects.js'
 import { HandlesEvents } from './features/support_events/handles_events.js'
@@ -23,7 +22,6 @@ export abstract class Component extends compose(
   HandlesEvents,
   HandlesRedirects,
   HandlesDecorators,
-  HandlesPageComponents,
   HandlesJsEvaluation,
   HandlesValidation
 ) {
